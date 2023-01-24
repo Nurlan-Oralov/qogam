20ed2090d66ed556bf655844c72298731df4bf61
Nurlan <oralovnur1@gmail.com>
2023-01-25 01:15:22 +0600

    User Authentication 0.11.1. patch. + Routes Setup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -104,3 +104,19 @@ func (app *application) deleteSnippet(w http.ResponseWriter, r http.Request) {
 	//айди взял а вот как делит сделать непонял((
 	fmt.Println(id)
 }
+
+func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display the user signup form...")
+}
+func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display the user login form...")
+}
+func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
