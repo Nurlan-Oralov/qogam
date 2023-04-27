cab9d5352224dc0ffef72d1712ea86ff48b32ac9
Nurlan <oralovnur1@gmail.com>
2023-04-28 04:07:42 +0600

    Testing 0.13.3. End-To-End Testing *Using httptest.Server *Using Test Helpers *Cookies and Redirections

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"testing"
+)
+
+// Create a newTestApplication helper which returns an instance of our
+// application struct containing mocked dependencies.
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		errorLog: log.New(io.Discard, "", 0),
+		infoLog:  log.New(io.Discard, "", 0),
+	}
+}
+
+// Define a custom testServer type which anonymously embeds a httptest.Server
+// instance.
+type testServer struct {
+	*httptest.Server
+}
+
+// Create a newTestServer helper which initalizes and returns a new instance
+// of our custom testServer type.
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	// Initialize a new cookie jar.
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Add the cookie jar to the client, so that response cookies are stored
+	// and then sent with subsequent requests.
+	ts.Client().Jar = jar
+	// Disable redirect-following for the client. Essentially this function
+	// is called after a 3xx response is received by the client, and returning
+	// the http.ErrUseLastResponse error forces it to immediately return the
+	// received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	return &testServer{ts}
+}
+
+// Implement a get method on our custom testServer type. This makes a GET
+// request to a given url path on the test server, and returns the response
+// status code, headers and body.
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return rs.StatusCode, rs.Header, body
+}
