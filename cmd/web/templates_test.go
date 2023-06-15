30974ce9eb23ffbcc8c6b33fd824196d8b5f8950
ZalibiZ123 <94436903+ZalibiZ123@users.noreply.github.com>
2023-06-15 14:02:45 +0600

    test

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestHumanDate(t *testing.T) {
+	// Create a slice of anonymous structs containing the test case name,
+	// input to our humanDate() function (the tm field), and expected output
+	// (the want field).
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.UTC),
+			want: "17 Dec 2020 at 10:00",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Dec 2020 at 09:00",
+		},
+		{
+			name: "WEST KAZAKHSTAN",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("UTC+5", 5*60*60)),
+			want: "17 Dec 2020 at 05:00",
+		},
+		{
+			name: "EAST KAZAKHSTAN",
+			tm:   time.Date(2020, 12, 17, 10, 0, 0, 0, time.FixedZone("UTC+6", 6*60*60)),
+			want: "17 Dec 2020 at 04:00",
+		},
+	}
+	// Loop over the test cases.
+	for _, tt := range tests {
+		// Use the t.Run() function to run a sub-test for each test case. The
+		// first parameter to this is the name of the test (which is used to
+		// identify the sub-test in any log output) and the second parameter is
+		// and anonymous function containing the actual test for each case.
+		t.Run(tt.name, func(t *testing.T) {
+			hd := humanDate(tt.tm)
+			if hd != tt.want {
+				t.Errorf("want %q; got %q", tt.want, hd)
+			}
+		})
+	}
+}
+
+func TestShowSnippet(t *testing.T) {
+	// Create a new instance of our application struct which uses the mocked
+	// dependencies.
+	app := newTestApplication(t)
+	// Establish a new test server for running end-to-end tests.
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	// Set up some table-driven tests to check the responses sent by our
+	// application for different URLs.
+	tests := []struct {
+		name     string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+		{"Zero ID", "/snippet/0", http.StatusNotFound, nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.get(t, tt.urlPath)
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q", tt.wantBody)
+			}
+		})
+	}
+}
